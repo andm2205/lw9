fb6e02d632c212aa0956ea0b6311c32522d68cfc
andm2205 <andm22056@gmail.com>
2026-06-17 22:24:51 +0700

    Add Profile screen

---
+++ app/src/main/java/com/example/navigationdemo/screens/Profile.kt
@@ -0,0 +1,21 @@
+package com.example.navigationdemo.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Profile(onClearBackStack: () -> Unit) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text("Setup your profile", style = MaterialTheme.typography.headlineSmall)
+            Spacer(modifier = Modifier.size(30.dp))
+            Button(onClick = { }) {
+                Text(text = "Go Home")
+            }
+        }
+    }
+}
