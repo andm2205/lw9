df560fb22708ab09e1ad8c00d2fade9520b56298
andm2205 <andm22056@gmail.com>
2026-06-17 22:21:57 +0700

    Add Welcome screen

---
+++ app/src/main/java/com/example/navigationdemo/screens/Welcome.kt
@@ -0,0 +1,22 @@
+package com.example.navigationdemo.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation3.runtime.NavKey
+
+@Composable
+fun Welcome(onNavigation: (NavKey) -> Unit, name: String = "") {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text("Welcome $name", style = MaterialTheme.typography.headlineSmall)
+            Spacer(modifier = Modifier.size(30.dp))
+            Button(onClick = { }) {
+                Text(text = "Set up your Profile")
+            }
+        }
+    }
+}
